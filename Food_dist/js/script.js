12e1993923b83d0758232091700b74bd973c9a40
miklay89 <miklay89@gmail.com>
2021-09-16 20:55:30 +0300

    modal window modification

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -97,13 +97,16 @@ window.addEventListener(`DOMContentLoaded`, () => {
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
     
-    modalTrigger.forEach((btn) => {
-
-    btn.addEventListener('click', () => {
+    function openModal() {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        });
+        clearInterval(modalTimerId);
+        }
+
+    modalTrigger.forEach((btn) => {
+
+    btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -125,5 +128,16 @@ window.addEventListener(`DOMContentLoaded`, () => {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll () {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+        
+    window.addEventListener('scroll', showModalByScroll);
 
 });
