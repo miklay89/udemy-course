4c342eca6144d715a00a3e63609d8d3af85fa4b5
miklay89 <miklay89@gmail.com>
2021-09-25 21:23:40 +0300

    reg exp

--- Food/js/script.js
+++ Food/js/script.js
@@ -358,12 +358,16 @@ window.addEventListener(`DOMContentLoaded`, () => {
             dots.push(dot);
         }
 
+        function deleteNoDigits(str) {
+            return +str.replace(/\D/g, '');
+        }
+
 
         next.addEventListener('click', () => {
-            if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+            if (offset == deleteNoDigits(width) * (slides.length - 1)){
                 offset = 0;
             } else {
-                offset += +width.slice(0, width.length - 2);
+                offset += deleteNoDigits(width);
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -385,9 +389,9 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
         prev.addEventListener('click', () => {
             if (offset == 0){
-                offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                offset = deleteNoDigits(width) * (slides.length - 1);
             } else {
-                offset -= +width.slice(0, width.length - 2);
+                offset -= deleteNoDigits(width);
             }
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -412,7 +416,7 @@ window.addEventListener(`DOMContentLoaded`, () => {
                 const slideTo = e.target.getAttribute('data-slide-to');
 
                 slideIndex = slideTo;
-                offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+                offset = deleteNoDigits(width) * (slideTo - 1);
 
                 slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -421,7 +425,7 @@ window.addEventListener(`DOMContentLoaded`, () => {
                 } else {
                     current.textContent = slideIndex;
                 }
-                
+
                 dots.forEach(dot => dot.style.opacity = '.5');
                 dots[slideIndex - 1].style.opacity = 1;
 
