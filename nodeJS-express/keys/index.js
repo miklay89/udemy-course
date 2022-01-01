009f029ea37ffd3a422f337560cfc60449188c11
M.Kurov <90147317+miklay89@users.noreply.github.com>
2022-01-01 20:32:20 +0300

    Delete nodeJS-express directory

--- nodeJS-express/keys/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://miklay:ENTuySS5AvAv9YW@cluster0.rsswf.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '',
-  EMAIL_FROM: 'mihail.kurov@yandex.ua',
-  BASE_URL: 'http://localhost:3000'
-};
--- nodeJS-express/utils/hbs-helpers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	ifeq(a, b, options) {
-		if (a == b) {
-			return options.fn(this);
-		} else {
-			return options.inverse(this);
-		}
-	}
-};
