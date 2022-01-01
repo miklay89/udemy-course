df2f307404d524baaf54a90bb784b9d61d176005
M.Kurov <90147317+miklay89@users.noreply.github.com>
2022-01-01 13:24:30 +0300

    Update index.js

--- nodeJS-express/keys/index.js
+++ nodeJS-express/keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   MONGODB_URI: 'mongodb+srv://miklay:ENTuySS5AvAv9YW@cluster0.rsswf.mongodb.net/shop',
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
+  SENDGRID_API_KEY: '',
   EMAIL_FROM: 'mihail.kurov@yandex.ua',
   BASE_URL: 'http://localhost:3000'
 };
